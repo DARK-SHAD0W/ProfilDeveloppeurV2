3790a52aa1a70cf956639abbe1eff0eb89f438dc
Ahmed <letaiefahmedyahya@gmail.com>
2026-07-08 10:52:20 +0200

    feat: definir des formes personnalisees (Shapes)

--- app/src/main/java/com/example/profildeveloppeurv2/ui/theme/Theme.kt
+++ app/src/main/java/com/example/profildeveloppeurv2/ui/theme/Theme.kt
@@ -1,10 +1,13 @@
 package com.example.profildeveloppeurv2.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
 
 private val LightColorScheme = lightColorScheme(
     primary = PrimaireClair,
@@ -26,6 +29,13 @@ private val DarkColorScheme = darkColorScheme(
     onSurface = OnSurfaceSombre
 )
 
+private val ProfilShapes = Shapes(
+    small = RoundedCornerShape(8.dp),
+    medium = RoundedCornerShape(16.dp),
+    large = RoundedCornerShape(24.dp),
+    extraLarge = RoundedCornerShape(32.dp)
+)
+
 @Composable
 fun ProfilDeveloppeurV2Theme(
     darkTheme: Boolean = isSystemInDarkTheme(),
@@ -40,6 +50,7 @@ fun ProfilDeveloppeurV2Theme(
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
+        shapes = ProfilShapes,
         content = content
     )
 }
